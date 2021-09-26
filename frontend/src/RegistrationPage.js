2a7f20a4b0c374f2ae455c7e9c20e4beda3160c8
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 20:22:35 -0700

    implemented the registration page

--- frontend/src/RegistrationPage.js
+++ frontend/src/RegistrationPage.js
@@ -3,7 +3,45 @@ import React, { useState } from "react";
 const RegistrationPage = () => {
   const initialState = { firstName: "", lastName: "", email: "", state: "" };
   const [formData, setFormData] = useState(initialState);
-  return <form></form>;
+  const handleChange = (evt) => {
+    setFormData((fData) => ({
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+  return (
+    <form>
+      <div>
+        <label htmlFor="firstName">First name: </label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last name: </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default RegistrationPage;
