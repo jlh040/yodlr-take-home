b79199f72d9ad0f16f579bf869bcc0ed1681b66e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 19:42:57 -0700

    implemented a basic form of navigation

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  return <p>Navigation</p>;
+  return (
+    <div>
+      <p>Navigation</p>
+      <p>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </p>
+      <p>
+        <NavLink to="/admin">Admin Page</NavLink>
+      </p>
+    </div>
+  );
 };
 
 export default Nav;
