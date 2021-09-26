726b08be65a5a6e9a33acbb615a72eba63dfa923
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 19:34:26 -0700

    created the Nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes />
     </div>
   );
