3c7da389b4602b2c43c44dddd08a68ed7229f472
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 21:27:09 -0700

    restyled the nav bar

--- frontend/src/NaviBar.js
+++ frontend/src/NaviBar.js
@@ -6,12 +6,20 @@ const NaviBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Yodlr Design Challenge</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Nav.Link exact to="/" as={NavLink}>
+            Yodlr Design Challenge
+          </Nav.Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link exact to="/admin" as={NavLink}>
+              Admin page
+            </Nav.Link>
+            <Nav.Link exact to="/signup" as={NavLink}>
+              Signup page
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
