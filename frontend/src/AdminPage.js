9afd299ac6f82f9711e7a395db2636cc84ef618a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-09-25 20:04:49 -0700

    users can now be displayed on the screen

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -4,7 +4,32 @@ import axios from "axios";
 
 const AdminPage = () => {
   const [allUsers, setAllUsers] = useState(null);
-  return <h2>Admin page</h2>;
+
+  useEffect(() => {
+    async function getUsers() {
+      const res = await axios.get("http://localhost:3001/users");
+      setAllUsers(res.data);
+    }
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2>Admin page</h2>
+      {allUsers ? (
+        allUsers.map((user) => (
+          <User
+            firstName={user.firstName}
+            lastName={user.lastName}
+            email={user.email}
+            state={user.state}
+          />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default AdminPage;
